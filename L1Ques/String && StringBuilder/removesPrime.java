4510c24da0765e0ad370d01ece212e4173cb1a65
vivmost <vivmost14@gmail.com>
2020-12-27 23:09:11 +0530

    more ques added

---
+++ L1Ques/String && StringBuilder/removesPrime.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void solution(ArrayList < Integer > al) {
+        // write your code here
+
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ArrayList < Integer > al = new ArrayList < > ();
+        for (int i = 0; i < n; i++) {
+            al.add(scn.nextInt());
+        }
+        solution(al);
+        System.out.println(al);
+    }
+
+}
