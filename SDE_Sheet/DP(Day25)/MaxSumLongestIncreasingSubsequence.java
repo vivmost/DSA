34b019c88e5b404c6ba8d42b9597b0d68637eea5
vivmost <vivmost@gmail.com>
2021-06-30 12:54:28 +0530

    Day_25(DP) -> solutions added

---
+++ SDE_Sheet/DP(Day25)/MaxSumLongestIncreasingSubsequence.java
@@ -0,0 +1,20 @@
+class Solution{
+	static int maxSumIS(int arr[], int n){
+        int i, j, max = 0;
+        int msis[] = new int[n];
+        
+        for (i = 0; i < n; i++)
+            msis[i] = arr[i];
+            
+        for (i = 1; i < n; i++)
+            for (j = 0; j < i; j++)
+                if (arr[i] > arr[j] && msis[i] < msis[j] + arr[i])
+                    msis[i] = msis[j] + arr[i];
+                    
+        for (i = 0; i < n; i++)
+            if (max < msis[i])
+                max = msis[i];
+
+        return max;
+    } 
+}
